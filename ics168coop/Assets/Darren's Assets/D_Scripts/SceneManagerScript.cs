c42093bce42c73403dc3e8d7eedbba47087db679
Darren <dartrenbbx@gmail.com>
2019-04-29 17:15:40 -0700

    Added scene manager

---
+++ ics168coop/Assets/Darren's Assets/D_Scripts/SceneManagerScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    private bool isPaused = false;
+    private bool isGameOver = false;
+    public GameObject PauseCanvas;
+    public GameObject GameOverCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        if (PauseCanvas == null || GameOverCanvas == null) return;
+        PauseCanvas.SetActive(false);
+        GameOverCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isGameOver && PauseCanvas != null)
+        {
+            PauseOrResume();
+        }
+    }
+
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void loadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void PauseOrResume()
+    {
+        try
+        {
+            if (!isPaused)
+            {
+                Time.timeScale = 0;
+                isPaused = true;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                isPaused = false;
+            }
+            PauseCanvas.SetActive(isPaused);
+        }
+        catch
+        {
+            Debug.Log("Esc No Work");
+        }
+    }
+
+    public void GameOver()
+    {
+        if (!isGameOver)
+        {
+            GameOverCanvas.SetActive(true);
+            isGameOver = true;
+            Time.timeScale = 0;
+        }
+    }
+}
